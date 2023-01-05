d17f7792e1715c93371f88dad204e1431fb2c7fa
yanovs <42386943+yanovs@users.noreply.github.com>
2023-01-05 12:16:59 -0500

    Update some functionality, including:
    - Add ability to set pos args sometimes
    - Add InternalStateField
    - Add str and compare fields
    - Improve str and ipy output
    - Add get_combined_metaclass helper
    - Add more tests

--- baseclasses/__init__.py
+++ baseclasses/__init__.py
@@ -1,8 +1,12 @@
 # flake8: noqa
 from baseclasses.core import (
+    MISSING,
     BaseClass,
     BaseMetaClass,
     Field,
+    InternalStateField,
     FrozenBaseClass,
     FrozenInstanceError,
+    get_combined_metaclass,
+    get_fields,
 )
--- baseclasses/core.py
+++ baseclasses/core.py
@@ -1,3 +1,15 @@
+"""Baseclasses.
+
+Like dataclasses but:
+  - No issues reordering and setting defaults on fields
+  - No redundant decorators (and no ability to make
+    child class not a baseclass)
+  - No magic str generation
+  - Can change frozen state per child class
+  - Can set `str` in addition to `repr`
+
+See also https://github.com/biqqles/dataclassy.
+"""
 import inspect
 import operator
 import types
@@ -30,6 +42,8 @@ MISSING = MissingType()
 _EMPTY_METADATA = types.MappingProxyType({})
 
 
+# TODO: Field.hash should return True if that's the default,
+#  instead of being None
 class Field:
 
     __slots__ = (
@@ -38,7 +52,9 @@ class Field:
         "default",
         "default_factory",
         "repr",
+        "str",
         "hash",
+        "compare",
         "metadata",
     )
 
@@ -46,8 +62,10 @@ class Field:
         self,
         default: Any = MISSING,
         default_factory: Callable[..., Any] = MISSING,
-        repr: bool = True,
-        hash: Optional[bool] = None,
+        repr: bool = True,  # noqa
+        str: bool = True,  # noqa
+        hash: Optional[bool] = None,  # noqa
+        compare: Optional[bool] = None,  # noqa
         metadata: Optional[Mapping[str, Any]] = None,
     ):
         if default is not MISSING and default_factory is not MISSING:
@@ -57,7 +75,9 @@ class Field:
         self.default = default
         self.default_factory = default_factory
         self.repr = repr
+        self.str = str
         self.hash = hash
+        self.compare = compare
         self.metadata = (
             _EMPTY_METADATA
             if metadata is None
@@ -66,14 +86,37 @@ class Field:
 
     def __repr__(self):
         return (
-            f"Field("
+            "Field("
             f"name={self.name!r},"
             f"type={self.type!r},"
             f"default={self.default!r},"
             f"default_factory={self.default_factory!r},"
             f"repr={self.repr!r},"
+            f"str={self.str!r},"
             f"hash={self.hash!r},"
-            f"metadata={self.metadata!r}"
+            f"compare={self.compare!r},"
+            f"metadata={self.metadata!r},"
+            ")"
+        )
+
+
+class InternalStateField(Field):
+    def __init__(
+        self,
+        default: Any = MISSING,
+        default_factory: Callable[..., Any] = MISSING,
+        metadata: Optional[Mapping[str, Any]] = None,
+    ):
+        # TODO: init=False
+        # TODO: serialize=False?
+        super().__init__(
+            default=default,
+            default_factory=default_factory,
+            repr=False,
+            str=False,
+            hash=False,
+            compare=False,
+            metadata=metadata,
         )
 
 
@@ -89,9 +132,12 @@ class BaseMetaClass(type):
         namespace: Dict[str, Any],
         **kwargs,
     ):
+        # Short-circuit oneself
         if name == "BaseClass" and namespace["__module__"] == Field.__module__:
             return type.__new__(mcs, name, bases, namespace)
 
+        # Gather all annotations and defaults
+        # across this class and its base classes
         all_annotations = {}
         all_defaults = {}
         frozen = False
@@ -123,9 +169,11 @@ class BaseMetaClass(type):
         namespace["__all_annotations__"] = all_annotations
         namespace["__all_defaults__"] = all_defaults
 
+        # Construct fields from all annotations
         fields = []
         for field_name, type_ in all_annotations.items():
             if field_name in all_defaults:
+                # noinspection PyUnresolvedReferences
                 default = all_defaults[field_name]
                 if isinstance(default, Field):
                     field = default
@@ -139,24 +187,66 @@ class BaseMetaClass(type):
             fields.append(field)
         namespace["__fields__"] = fields
 
+        # Class state
         namespace["__frozen__"] = kwargs.get("frozen", frozen)
+
+        # Signature for ipy help
+        signature_parameters = []
+        for field in fields:
+            parameter_kwargs = {}
+            if field.default is not MISSING:
+                parameter_kwargs["default"] = field.default
+            if field.type is not None:
+                parameter_kwargs["annotation"] = field.type
+
+            parameter = inspect.Parameter(
+                field.name, inspect.Parameter.KEYWORD_ONLY, **parameter_kwargs
+            )
+            signature_parameters.append(parameter)
+        namespace["__signature__"] = inspect.Signature(
+            parameters=signature_parameters, return_annotation=None
+        )
 
         return type.__new__(mcs, name, bases, namespace)
 
 
 class BaseClass(metaclass=BaseMetaClass):
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
+        if args:
+            if set(self.__class__.__bases__) - {BaseClass, FrozenBaseClass}:
+                raise TypeError(
+                    f"Cannot use positional args when "
+                    f"using inheritance: {self.__class__.__name__}"
+                )
+
+            kwargs_from_args = {}
+            # noinspection PyUnresolvedReferences
+            for arg, field in zip(
+                args, self.__class__.__fields__
+            ):  # type: Field
+                if field.name in kwargs:
+                    raise TypeError(
+                        f"Cannot use both positional and "
+                        f"keyword args for {field.name!r}: "
+                        f"{self.__class__.__name__}"
+                    )
+
+                kwargs_from_args[field.name] = arg
+
+            kwargs_from_args.update(kwargs)
+            kwargs = kwargs_from_args
+
         self.__pre_init__(kwargs)
 
+        # noinspection PyUnresolvedReferences
         for field in self.__class__.__fields__:  # type: Field
             if field.name in kwargs:
                 self.__dict__[field.name] = kwargs[field.name]
             elif field.default_factory is not MISSING:
-                # TODO: Find a better way of doing this
-                # TODO: Also, this only passes in non-default kwargs
                 if (
                     inspect.isfunction(field.default_factory)
                     and field.default_factory.__name__ == "<lambda>"
+                    and inspect.getfullargspec(field.default_factory).varkw
                 ):
                     self.__dict__[field.name] = field.default_factory(**kwargs)
                 else:
@@ -171,7 +261,10 @@ class BaseClass(metaclass=BaseMetaClass):
 
         unexpected_kwargs = set(kwargs.keys()) - set(self.__dict__.keys())
         if unexpected_kwargs:
-            raise TypeError(f"Unexpected arguments: {unexpected_kwargs}")
+            raise TypeError(
+                f"{self.__class__.__name__}.__init__() "
+                f"unexpected arguments: {unexpected_kwargs}"
+            )
 
         self.__post_init__()
 
@@ -182,25 +275,23 @@ class BaseClass(metaclass=BaseMetaClass):
         pass
 
     def __setattr__(self, key: str, value: Any):
+        # noinspection PyUnresolvedReferences
         if self.__class__.__frozen__:
             raise FrozenInstanceError(f"Cannot assign to field {key!r}")
 
         super().__setattr__(key, value)
 
-    def __hash__(self):
+    def __hash__(self) -> int:
+        # noinspection PyUnresolvedReferences
         if not self.__class__.__frozen__:
             return NotImplemented
 
-        as_tuple = tuple(
-            getattr(self, field.name)
-            for field in self.get_fields()
-            if field.hash is None or field.hash
-        )
-        return hash(as_tuple)
+        return hash(self._as_hash_tuple())
 
     def _compare(self, other: Any, op: Callable[[Any, Any], bool]) -> bool:
         if other.__class__ is self.__class__:
-            return op(self.as_tuple(), other.as_tuple())
+            # noinspection PyProtectedMember
+            return op(self._as_compare_tuple(), other._as_compare_tuple())
 
         return NotImplemented
 
@@ -229,17 +320,97 @@ class BaseClass(metaclass=BaseMetaClass):
         )
         return f"{self.__class__.__qualname__}({inner})"
 
+    def __str__(self) -> str:
+        # We need to this complicated logic because str(list) in python
+        # actually calls repr() on each element
+        # (see https://stackoverflow.com/a/12448200).
+        # NB: The logic below wouldn't work with
+        # recursive structures (e.g., list of list).
+        inner_parts = []
+        for field in self.get_fields():
+            if not field.str:
+                continue
+
+            field_value = getattr(self, field.name)
+            # Explicitly call str() on elements of collection
+            # (instead of repr())
+            if isinstance(field_value, tuple):
+                field_value = (
+                    "(" + ", ".join(str(elem) for elem in field_value) + ")"
+                )
+            elif isinstance(field_value, list):
+                field_value = (
+                    "[" + ", ".join(str(elem) for elem in field_value) + "]"
+                )
+            elif isinstance(field_value, dict):
+                field_value = (
+                    "{"
+                    + ", ".join(
+                        f"{key!r}: {str(value)}"
+                        for key, value in field_value.items()
+                    )
+                    + "}"
+                )
+            # Don't recurse down other baseclasses
+            elif isinstance(field_value, BaseClass):
+                field_value = f"{field_value.__class__.__name__}(...)"
+            else:
+                field_value = str(field_value)
+
+            inner_parts.append(f"{field.name}={field_value}")
+
+        return f"{self.__class__.__qualname__}({', '.join(inner_parts)})"
+
+    # For prettier IPython output
+    def _repr_pretty_(
+        self, printer, cycle: bool  # IPython.lib.pretty.PrettyPrinter
+    ):
+        """Route pretty repr printing to str instead of repr.
+
+        Adapted from https://stackoverflow.com/a/41454816."""
+        printer.text(str(self) if not cycle else "...")
+
+    def _as_hash_tuple(self) -> Tuple[Any, ...]:
+        return tuple(
+            getattr(self, field.name)
+            for field in self.get_fields()
+            if field.hash is None or field.hash
+        )
+
+    def _as_compare_tuple(self) -> Tuple[Any, ...]:
+        return tuple(
+            getattr(self, field.name)
+            for field in self.get_fields()
+            if field.compare is None or field.compare
+        )
+
     def as_tuple(self) -> Tuple[Any, ...]:
         return tuple(getattr(self, field.name) for field in self.get_fields())
 
     def as_dict(self) -> Dict[str, Any]:
         return {
-            field.name: getattr(self, field.name) for field in self.get_field()
+            field.name: getattr(self, field.name)
+            for field in self.get_fields()
         }
 
     def get_fields(self) -> Sequence[Field]:
+        # noinspection PyUnresolvedReferences
         return self.__class__.__fields__
 
 
 class FrozenBaseClass(BaseClass, frozen=True):
     pass
+
+
+def get_combined_metaclass(cls: Type, frozen: bool = False) -> Type:
+    base_cls = FrozenBaseClass if frozen else BaseClass
+
+    class CombinedMetaClass(type(base_cls), type(cls)):
+        pass
+
+    return CombinedMetaClass
+
+
+def get_fields(cls: Type[BaseClass]) -> Sequence[Field]:
+    # noinspection PyUnresolvedReferences
+    return cls.__fields__
