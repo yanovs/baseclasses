42f9cdd377e7e7d9ba2cc39b94ea445ead700c2e
yanovs <42386943+yanovs@users.noreply.github.com>
2023-05-14 19:53:17 -0400

    Export public API properly (#2)
    * Export public API properly (via redundant imports)

--- baseclasses/__init__.py
+++ baseclasses/__init__.py
@@ -1,12 +1,10 @@
 # flake8: noqa
-from baseclasses.core import (
-    MISSING,
-    BaseClass,
-    BaseMetaClass,
-    Field,
-    FrozenBaseClass,
-    FrozenInstanceError,
-    InternalStateField,
-    get_combined_metaclass,
-    get_fields,
-)
+from baseclasses.core import MISSING as MISSING
+from baseclasses.core import BaseClass as BaseClass
+from baseclasses.core import BaseMetaClass as BaseMetaClass
+from baseclasses.core import Field as Field
+from baseclasses.core import FrozenBaseClass as FrozenBaseClass
+from baseclasses.core import FrozenInstanceError as FrozenInstanceError
+from baseclasses.core import InternalStateField as InternalStateField
+from baseclasses.core import get_combined_metaclass as get_combined_metaclass
+from baseclasses.core import get_fields as get_fields
